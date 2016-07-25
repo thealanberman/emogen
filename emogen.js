3762a6c96abeb241949a66f9a7c51e4552a87b44
thealanberman <thealanberman@gmail.com>
2016-07-25 14:52:15 +0000

    changed some variable names

--- emogen.js
+++ emogen.js
@@ -2,13 +2,13 @@ window.onload = initAll;
 
 function initAll() {
   document.getElementById("fullName").select();
-  document.getElementById("sillySubmit").onclick = function() {
-    document.getElementById("msgField").innerHTML = getSillyName();
+  document.getElementById("emoSubmit").onclick = function() {
+    document.getElementById("msgField").innerHTML = getEmoBandName();
     return false;
   }
 }
 
-function getSillyName() {
+function getEmoBandName() {
   var joiners = ["The", "At", "In", "And", "On", "Of", "With"];
   var prefixes = ["Hey", "My", "Your", "Our", "The", "Between", "Since The", "A", "And", "New", "Old", "At", "In", "Into", "Inside", "Near", "Of", "On", "Outside", "Over", "With", "Within", "Without", "Around", "Besides", "Beside", "After", "Against", "Off", "During", "Down", "Like", "Near", "Along", "Beneath", "Under", "Toward", "Till", "Up", "Until", "Through", "Throughout", "Except", "For", "From", "Past", "Burning", "Sleeping", "Further"];
   var nouns = ["Ring", "Promise", "Texas", "New York", "Music", "Hearts", "Kids", "Surprise", "Reason", "Kentucky", "Oklahoma", "Mississippi", "Rangers", "Fence", "Cap'n", "Mandy", "Chester", "Jimmy", "Johnny", "Jazz", "Experiment", "Drive", "North", "South", "East", "West", "Coalition", "New", "Boys", "Girls", "Plan", "Disco", "Star", "Maplewood", "Orange", "Mercedes", "Omaha", "Hoboken", "Newark", "Ho-Ho-Kus", "Second", "Asbury", "Trenton", "Cherry Hill", "Morrisville", "Elizabeth", "Morristown", "Clifton", "Hackettstown", "Theory", "Juliana", "Joan", "Parade", "Romance", "Hayward", "Fremont", "Pittsburg", "Josie", "Ethel", "Aberdeen", "Barnegat", "Belleville", "Bergenfield", "Berkeley", "Bloomfield", "Camden", "Cliffside Park", "Edison", "Englewood", "Fort Lee", "Jackson", "Keansburg", "Kearny", "Hackensack", "Glen Rock", "Middlesex", "Millburn", "Montclair", "Lumberton", "Lyndhurst", "Passaic", "Roseville", "Pine Hill", "Rutherford", "Saddle Brook", "Teaneck", "Secaucus", "Warren", "Wayne", "Weehawken", "Winslow", "Woodbridge", "Windsor"];
